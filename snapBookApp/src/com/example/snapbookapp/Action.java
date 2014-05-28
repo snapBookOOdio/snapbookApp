919e9889fd2c816de35606965e6c98be965b2908
snapBookOOdio <Snapchai1@Yellow.local>
2014-05-28 09:44:06 +0530

    remove

--- snapBookApp/src/com/example/snapbookapp/Action.java
+++
@@ -1,5 +0,0 @@
-package com.example.snapbookapp;
-
-public class Action {
-	public static final String ACTION_MULTIPLE_PICK = "Galary1.ACTION_MULTIPLE_PICK";
-}
--- snapBookApp/src/com/example/snapbookapp/CustomGallery.java
+++
@@ -1,8 +0,0 @@
-package com.example.snapbookapp;
-
-public class CustomGallery {
-
-	public String sdcardPath;
-	public boolean isSeleted = false;
-
-}
